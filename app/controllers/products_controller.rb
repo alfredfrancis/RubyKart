47341ac769ae48ad620755ed9a72bad109f1f8b6
alfred francis <alfredfranciz@gmail.com>
2015-12-28 19:30:53 +0530

    added products show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,14 @@
 class ProductsController < ApplicationController
 	def index
 		@categories = Category.all
-		#binding.pry
 		if(params[:category_id])
 			@products = Product.where(category: params[:category_id])
 		else	
 			@products = Product.all
 		end
+	end
+
+	def show
+		@product = Product.find(params[:id])	
 	end
 end
